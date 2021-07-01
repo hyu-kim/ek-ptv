b7ae6d7f8e3e2362079774518107e2edbe17490e
Hyu Kim <hskim0000@gmail.com>
2021-06-30 22:00:15 -0400

    testing adjusted images

--- main.py
+++ main.py
@@ -3,6 +3,41 @@
 """
 Created on Mon Jun 28 02:46:22 2021
 
-@author: hk
+@author: Hyu Kim (hskimm@mit.edu)
 """
+from __future__ import division, unicode_literals, print_function  # for compatibility with Python 2 and 3
 
+import os
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series  # for convenience
+
+import pims
+import trackpy as tp
+import time
+
+# Optionally, tweak styles.
+mpl.rc('figure',  figsize=(10, 5))
+mpl.rc('image', cmap='gray')
+
+os.chdir('/Volumes/LEMI_HK/LLNL BioSFA/EK/2021-04-25/tiff')
+
+s = '+1R1_R1_Ch05_TxRed_10-60V_1Vps_10X_001.ome';
+i = 410;
+frame = pims.open('%s.tif' % s);
+# i = 0;
+# frame = pims.open('+1R1_R1_Ch05_TxRed_10-60V_1Vps_10X_001_301.jpg');
+
+# frame_tmp = np.reshape(frame, (1,-1))
+# h = plt.hist(frame_tmp[0], bins='auto')
+
+diam = 51;
+
+t1 = time.time();
+# f = tp.locate(frame[i], diam, separation=1, percentile=80, minmass=100, threshold=15, invert=False);
+f = tp.batch(frame, diam, minmass=100);
+t2 = time.time();
+# tp.annotate(f, frame[i])
+print("elapsed : %s sec" % (t2-t1));
