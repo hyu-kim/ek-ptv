c6b9e56df9ec2872e5eeef77f215dad1652beef7
Hyu Kim <hskim0000@gmail.com>
2021-07-01 02:25:49 -0400

    made a change

--- main.py
+++ main.py
@@ -23,21 +23,23 @@ mpl.rc('figure',  figsize=(10, 5))
 mpl.rc('image', cmap='gray')
 
 os.chdir('/Volumes/LEMI_HK/LLNL BioSFA/EK/2021-04-25/tiff')
+# os.chdir('./+1R1_R1-1');
 
 s = '+1R1_R1_Ch05_TxRed_10-60V_1Vps_10X_001.ome';
 i = 410;
 frame = pims.open('%s.tif' % s);
 # i = 0;
-# frame = pims.open('+1R1_R1_Ch05_TxRed_10-60V_1Vps_10X_001_301.jpg');
+# frame = pims.open('+1R1_R1_Ch05_TxRed_10-60V_1Vps_10X_001.ome0401.jpg');
 
 # frame_tmp = np.reshape(frame, (1,-1))
 # h = plt.hist(frame_tmp[0], bins='auto')
 
-diam = 51;
+diam = 31;
 
 t1 = time.time();
 # f = tp.locate(frame[i], diam, separation=1, percentile=80, minmass=100, threshold=15, invert=False);
-f = tp.batch(frame, diam, minmass=100);
+f = tp.locate(frame[i], diam, invert=False);
+# f = tp.batch(frame, diam, minmass=100);
 t2 = time.time();
-# tp.annotate(f, frame[i])
+tp.annotate(f, frame[i])
 print("elapsed : %s sec" % (t2-t1));
