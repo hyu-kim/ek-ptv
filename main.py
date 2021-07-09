0bb63df648457d9b4123c05111bf90045a3d5b07
Hyu Kim <hskim0000@gmail.com>
2021-07-09 12:25:41 -0400

    Jul09

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series  # for convenience
-
+from sub import trans_contrast
 import pims
 import trackpy as tp
 import time
@@ -22,24 +22,21 @@ import time
 mpl.rc('figure',  figsize=(10, 5))
 mpl.rc('image', cmap='gray')
 
-os.chdir('/Volumes/LEMI_HK/LLNL BioSFA/EK/2021-04-25/tiff')
-# os.chdir('./+1R1_R1-1');
+# os.chdir('/Volumes/LEMI_HK/LLNL BioSFA/EK/2021-04-25/tiff')
+os.chdir('/Users/hk/Desktop/LEMI/SFA/Electrokinetics/trackpy')
 
 s = '+1R1_R1_Ch05_TxRed_10-60V_1Vps_10X_001.ome';
-i = 410;
 frame = pims.open('%s.tif' % s);
-# i = 0;
-# frame = pims.open('+1R1_R1_Ch05_TxRed_10-60V_1Vps_10X_001.ome0401.jpg');
+frame = trans_contrast(frame, q1=0.7, q2=1-1e-3)
 
-# frame_tmp = np.reshape(frame, (1,-1))
-# h = plt.hist(frame_tmp[0], bins='auto')
-
-diam = 31;
+diam = 35;
+i = 49;
+print("Line 34");
 
 t1 = time.time();
-# f = tp.locate(frame[i], diam, separation=1, percentile=80, minmass=100, threshold=15, invert=False);
-f = tp.locate(frame[i], diam, invert=False);
-# f = tp.batch(frame, diam, minmass=100);
+# f = tp.locate(frame[i], diam, invert=False, topn=20);
+f = tp.batch(frame, diam, topn=20);
+tp.quiet()
 t2 = time.time();
 tp.annotate(f, frame[i])
 print("elapsed : %s sec" % (t2-t1));
