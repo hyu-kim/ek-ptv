a226b31fe066a782a7a06c3d783c1bb4214ab104
Hyu Kim <61758741+hyu-kim@users.noreply.github.com>
2022-01-27 13:02:46 -0500

    pH info excluded for GSP2022

--- R/create_df.R
+++ R/create_df.R
@@ -6,27 +6,26 @@ library(tidyverse)
 rm(list=ls())
 
 ## import and clean
-date = '2021-12-22'
-path <- paste("/Users/hyungseokkim/Desktop/LEMI/SFA/Electrokinetics/", date, " Pt mobility 3", sep="")
+date = '2021-12-31'
+path <- paste("/Users/hyungseokkim/Desktop/LEMI/SFA/Electrokinetics/", date, " Pt mobility 4", sep="")
 setwd(path)
 filename = paste("info_",date,".txt",sep="")
 rd_info = read.delim(filename,sep = ",",header=TRUE,dec = ".")
 
 setwd(paste(path,"vy",sep="/"))
-df = data.frame(Treatment=factor(), Replicate=factor(), Voltage=double(), pH=double(), Velocity=double()) # use only when beginning from scratch
+df = data.frame(Date=factor(), Treatment=factor(), Replicate=factor(), Voltage=double(), Velocity=double()) # use only when beginning from scratch
 for (ind in c(1:dim(rd_info)[1])) {
   if (rd_info$'channel'[ind]>14) next # consider QW#1 device only this time
-  str = sprintf("%s_R%d_Ch%02d_pH%dp%02d_%s_%02dV_10X_001.ome.txt", rd_info$'cond'[ind], 
-                rd_info$'rep'[ind], rd_info$'channel'[ind], floor(rd_info$'ph'[ind]),
-                round(100*(rd_info$'ph'[ind] - floor(rd_info$'ph'[ind]))), 
-                rd_info$'light'[ind], rd_info$'voltage'[ind])
+  str = sprintf("%s_R%d_Ch%02d_TR_%02dV_10X_001.ome.csv", rd_info$'cond'[ind], 
+                rd_info$'rep'[ind], rd_info$'channel'[ind], 
+                rd_info$'voltage'[ind])
   rd = read.delim(str, header=TRUE, dec = ".")
   for (i in 1:dim(rd)[1]) {
     df_temp <- data.frame(
+      Date = rd_info$'date'[ind],
       Treatment = rd_info$'cond'[ind],
       Replicate = rd_info$'rep'[ind],
       Voltage = rd_info$'voltage'[ind],
-      pH = rd_info$'ph'[ind],
       Velocity = rd$'velocity'[i])
     df <- rbind(df, df_temp)
   }
--- R/plot_all.R
+++ R/plot_all.R
@@ -1,66 +1,55 @@
-# Created by Hyungseok Kim (hskimm@mit.edu), last modified on 1/18/22
+# Created by Hyungseok Kim (hskimm@mit.edu), last modified on 1/27/22
 # Continued from "create_df.R" THEN "lm.R"
-# Plots mean+-sd of EK mobility by each treatment
 library("ggplot2")
 library(tidyverse)
 library(Rmisc)
 
-# mu_df_SE <- summarySE(
-#   mu_df, 
-#   measurevar = 'Mobility', groupvars = 'Treatment'
-#   ) # summarizes df by each treatment and replicate
+# ### add a column with a date
+# mu_df$date <- date
+# write.csv(mu_df, paste(path,"/",date,"_mu.csv",sep=""),row.names=FALSE)
 # 
-# mu_df_SE %>%
-#   mutate(Treatment = factor(Treatment, levels=trts)) %>%
-#   ggplot( aes(x=Treatment, y=Mobility * 1e9)) +
-#     geom_bar(stat="identity", position=position_dodge(), fill=NA, colour='black') + 
-#     # geom_errorbar(aes(ymin=(Mobility-sd)*1e9, ymax=(Mobility+sd)*1e9), width=.2) + 
-#     geom_point(stroke = 0.2, size = 1, pch = 21, position = position_jitterdodge(0.3)) +
-#     labs(x='Strain', y=expression(paste('Mobility (','10'^{-9}, ' m'^2,'/ V-s)')))
+# ### combine two dates and plot
+# mu_df_all <- data.frame(matrix(rep(0,4), nrow=1))
+# names(mu_df_all) <- colnames(mu_df)
+# for (date in dates){
+#   mu_df_temp = read.csv(paste(path,"/",date,"_mu.csv",sep=""))
+#   mu_df_all <- rbind(mu_df_all, mu_df_temp)
+# }
 
-mu_df %>%
-  mutate(Treatment = factor(Treatment, levels=trts)) %>%
-  mutate(Replicate = factor(Replicate, levels=reps)) %>%
-  # ggplot(aes(x=pH, y=Mobility * 1e9, fill=Replicate)) + 
-  ggplot(aes(x=pH, y=Mobility * 1e9)) + 
-  # geom_bar(stat = "summary", fun = mean, size = 0.2, width = 0.75, fill=NA, colour='black') +
-  geom_line() + geom_point() +
-  # geom_point(stroke = 0.2, size = 1, pch = 21, position = position_jitterdodge(0.3)) +
-  labs(x='pH', y=expression(paste('Mobility (','10'^{-9}, ' m'^2,'/ V-s)')))
-
-setwd(paste(path,"figs",sep="/"))
-ggsave('mobility.eps', width = 4, height = 3.5, units = "in")
-
-### add a column with a date
-mu_df$date <- date
-write.csv(mu_df, paste(path,"/",date,"_mu.csv",sep=""),row.names=FALSE)
-
-### combine two dates and plot
-setwd("/Users/hk/Desktop/LEMI/SFA/Electrokinetics/codes")
-path <- getwd()
-dates = c('2021-12-22', '2021-12-31')
-mu_df_all <- data.frame(matrix(rep(0,4), nrow=1))
-names(mu_df_all) <- colnames(mu_df)
-for (date in dates){
-  mu_df_temp = read.csv(paste(path,"/",date,"_mu.csv",sep=""))
-  mu_df_all <- rbind(mu_df_all, mu_df_temp)
-}
-
-mu_df_all <- mu_df_all[-1,]
+mu_df_all <- mu_df
 mu_df_all$Treatment[mu_df_all$Treatment=='Ax'] <- 'Axenic'
-mu_df_all$Treatment[mu_df_all$Treatment=='+1R1'] <- '+ARW1R1'
-mu_df_all$date[mu_df_all$date=='2021-12-22'] <- 'log (5 d)'
-mu_df_all$date[mu_df_all$date=='2021-12-31'] <- 'stat (14 d)'
+mu_df_all$Treatment[mu_df_all$Treatment=='+1R1'] <- '+ Algori.'
+mu_df_all$Treatment[mu_df_all$Treatment=='+13C1'] <- '+ Labren.'
+mu_df_all$date[mu_df_all$Date=='2021-12-22'] <- 'Exp. (5 d)'
+mu_df_all$date[mu_df_all$Date=='2021-12-31'] <- 'Stat. (14 d)'
 
 mu_df_all %>%
-  mutate(Treatment = factor(Treatment, levels=c('Axenic', '+13C1', '+ARW1R1'))) %>%
+  mutate(Treatment = factor(Treatment, levels=c('Axenic', '+ Labren.', '+ Algori.'))) %>%
   mutate(Replicate = factor(Replicate, levels=reps)) %>%
   ggplot() + 
-  geom_bar(stat = "summary", fun = mean, size = 0.2, width = 0.75, 
+  geom_bar(stat = "summary", fun = mean, size = 0.3, width = 0.75, color = 'black',
            position = position_dodge(),
            aes(x=Treatment, y=Mobility * 1e9, fill=date)) +
-  geom_point(stroke = 0.2, size = 1, pch = 21, position = position_jitterdodge(0.1),
-             aes(x=Treatment, y=Mobility * 1e9, fill=date)) +
-  labs(x='Culture', y=expression(paste('Mobility (','10'^{-9}, ' m'^2,'/ V-s)')), fill='Growth stage')
+  geom_point(stroke = 0.8, size = 2, pch = 16, position = position_jitterdodge(0.1),
+             aes(x=Treatment, y=Mobility * 1e9, color='black', group=date)) +
+  scale_fill_manual(values=c("#A6C062","#015484")) +
+  theme(text = element_text(size=18),
+        title =element_text(size=18),
+        legend.position = "bottom", 
+        # axis.text.y = element_blank(),
+        axis.text.x = element_text(size=18),
+        # axis.title.y  = element_blank(),
+        axis.title.x  = element_blank(),
+        axis.ticks.y = element_line(colour = "black", size = 0.5),
+        axis.ticks.x = element_line(colour = "black", size = 0.5),
+        strip.background = element_rect(fill=NA),
+        strip.text = element_blank(),
+        panel.background = element_rect(fill = "transparent"),
+        panel.grid.major = element_line(colour='grey', size=0.5), 
+        panel.grid.minor = element_blank(),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        panel.border = element_rect(colour = "black", fill=NA, size=0.5)) +
+  labs(x='Culture', y=expression(paste('Electrokinetic mobility (','10'^{-9}, ' m'^2,'/ V-s)')), fill='Growth stage')
 
-ggsave('mobility_all.eps', width = 6, height = 3.5, units = "in")
+setwd("/Users/hyungseokkim/Desktop/LEMI/SFA/Conference/2022-02 GSP/img+data")
+ggsave('ek_mobility.eps', width = 6, height = 6, units = "in")
