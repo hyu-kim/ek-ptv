9ea776c86383b4c47a6140e81b7942f2df1f9960
Hyu Kim <61758741+hyu-kim@users.noreply.github.com>
2022-02-11 08:52:03 -0500

    brought from amp (ph ver.)

--- R/lm.R
+++ R/lm.R
@@ -4,8 +4,6 @@
 # Parameters = Voltage (float), Replicate (categorical), (Treatment (categorical))
 
 df$"Replicate" <- as.factor(df$"Replicate") # Replicate into categorical variables
-df$Date <- as.factor(df$Date)
-df$Treatment <- as.factor(df$Treatment)
 
 eps_r = 80; # relative permitivity []
 eps_0 = 8.854e-12; # vacuum permitivity [F/m]
@@ -13,29 +11,27 @@ eta = 8.66e-4; # viscosity [Pa-s]
 l = 10e-3; # channel length [m]
 
 # subset df and loop
-dates = c('2021-12-22', '2021-12-31')
-trts = c('Ax', '+13C1', '+1R1')
+trts = c('Ax')
 reps = c(1,2,3)
+phs = c(7.76, 8, 8.21, 9.16, 9.43, 8.57, 9.69, 9.94)
 
-mu_df <- data.frame(Date=factor(), Treatment=factor(), Replicate=factor(), Mobility=double())
+mu_df <- data.frame(Treatment=factor(), Replicate=factor(), pH=double(), Mobility=double())
 for (trt in trts){ # for passing into "plot_all.R"
-  for (d in dates){
-    for (rep in reps){
-      df_params <- df[(df$Treatment==trt)&(df$Date==d)&(df$Replicate==rep),]
-      res <- lm(Velocity ~ Voltage, data = df_params)
-      print(summary(res))
-      coef <- res$coefficients[[2]] # [µm / V-s] 
-      mu <- coef * l * 1e-6 # [m2 / V-s]
-      mu_df_temp <- data.frame(
-        Date = d,
-        Treatment = trt,
-        Replicate = rep,
-        Mobility = mu
-        )
-      mu_df <- rbind(mu_df, mu_df_temp)
-    }
+  # for (rep in reps){
+  for (ph in c(7.76)){
+    rep <- rd_info$rep[rd_info$ph==ph][1]
+    df_params <- df[(df$Treatment==trt)&(df$pH==ph),]
+    res <- lm(Velocity ~ Voltage, data = df_params)
+    print(summary(res))
+    coef <- res$coefficients[[2]] # [µm / V-s] 
+    mu <- coef * l * 1e-6 # [m2 / V-s]
+    mu_df_temp <- data.frame(
+      Treatment = trt,
+      Replicate = rep,
+      pH = ph,
+      Mobility = mu
+    )
+    mu_df <- rbind(mu_df, mu_df_temp)
   }
+  # }
 }
-
-# Export mu_df into csv
-write.csv(mu_df, paste(path,"mu_df.csv",sep='/'), row.names = FALSE)
