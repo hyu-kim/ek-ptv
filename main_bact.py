78f45f5421ee392df4f66094883e8f44ddd8dc59
Hyu Kim <61758741+hyu-kim@users.noreply.github.com>
2022-02-18 00:44:12 -0500

    Update main_bact.py

--- main_bact.py
+++ main_bact.py
@@ -11,6 +11,7 @@ Dec 04: Use v_y instead of mobility for export
 Dec 11: Export trace dataframe for record. Increase the upper cutoff filtering velocity range
 Dec 17: 1) updated module sub ("get_v" and "convert_tr")
         2) Hold back using kmeans clustering -- as the mobility computed too high than expected
+Feb 18: Bring code from "main_pt.py"
 
 @author: Hyu Kim (hskimm@mit.edu)
 """
@@ -26,16 +27,17 @@ import trackpy as tp
 import time
 
 # %%
-exp_date = '2021-12-09'
-path_info = '/Users/hk/Desktop/LEMI/DEP-LPS/Linear EK/info_' + exp_date + '.txt'
-path_plot = '/Users/hk/Desktop/LEMI/DEP-LPS/Linear EK/analysis'
+exp_date = '2022-02-16'
+path_info = '/Users/hk/Desktop/LEMI/SFA/Electrokinetics/' + exp_date + ' bact ek/' + 'info_' + exp_date + '.txt'
+path_plot = '/Users/hk/Desktop/LEMI/SFA/Electrokinetics/' + exp_date + ' bact ek/'
+path_sav_vy = path_plot + 'vy/'
+path_sav_tr = path_plot + 'tr/'
 info = pd.read_csv(path_info, delimiter=',', header=0)
 
-path_tif = '/Volumes/LEMI_HK/LPS-DEP/XXXX-XX-XX/adjusted'
-for i in range(34,54):
+path_tif = '/Volumes/LEMI_HK/LLNL BioSFA/EK/XXXX-XX-XX/adjusted'
+for i in range(34,41):
     path_tif = path_tif.replace('XXXX-XX-XX',info.date[i])
-    s = path_tif + '/' + '%s_R%d_Ch%02d_GFP_%02dV_20X_001.ome.tif' % (info.cond[i], info.rep[i], info.channel[i], info.voltage[i])
-    # s = path_tif + '/' + '%s_R%d_Ch%02d_GFP_%02dV_20X_001.ome_test.tif' % (info.cond[i], info.rep[i], info.channel[i], info.voltage[i])
+    s = path_tif + '/' + '%s_R%d_Ch%02d_pH%dp%02d_%s_%02dV_10X_001.ome.tif' % (info.cond[i], info.rep[i], info.channel[i], math.floor(info.ph[i]), round(100*(info.ph[i]-math.floor(info.ph[i]))), info.light[i], info.voltage[i])
     frame = pims.open(s)
 
     t1 = time.time()
@@ -44,13 +46,15 @@ for i in range(34,54):
     cnt = cnt * ((cnt>=10) & (cnt<=200)) + 10 * ((cnt<10) | (cnt>200))
     frame2, _ = sub.binarize_batch(frame) # for validating tp.annotate
 
-    f = pile(frame[1:], topn=cnt//2) # exclude the first frame it has been subtracted to remove background
+    f = pile(frame[1:], topn=cnt*2//3) # exclude the first frame it has been subtracted to remove background
     # f = pile(frame[1:], topn=5) # use this when cell number is too low (i.e. wrong cnt)
     # tp.annotate(f[100], frame2[100]) # run this to check if cells were properly detected
 
     pred = tp.predict.NearestVelocityPredict()
     tr = pd.concat(pred.link_df_iter(f, search_range=25))
-    
+    # tr = tp.link(f, 50); # not anymore
+    # tr = tr[(tr['x'] < x_hi) & (tr['x'] > x_lo)]; # not recommended to use
+
     tr = sub.filter_ephemeral(tr, thres=5)
 
     tr_v = sub.get_v(tr)
@@ -59,21 +63,21 @@ for i in range(34,54):
     t2 = time.time()
     print("elapsed : %s sec" % (t2-t1))
 
-    tr_v2 = sub.filter_v(tr_v, xlim=10, ylim1=2*info.voltage[i], ylim2=-10, direction=True)
+    tr_v2 = sub.filter_v(tr_v, xlim=10, ylim1=info.voltage[i], ylim2=-10, direction=True)
     sub.plot_tr_v(tr_v2)
 
-    tr_v3 = sub.convert_tr(tr_v2, front=info.front[i], back=info.back[i], rate_time=1/info.fps[i], rate_space=1)
+    info = pd.read_csv(path_info, delimiter=',', header=0) # update info
+    tr_v3 = sub.convert_tr(tr_v2, front=info.front[i], back=info.back[i], rate_time=1/info.fps[i], rate_space=1.29) # mag 10x, binned 2 by 2
 
     tr_av = sub.each_particle(tr_v3, vol_init=info.voltage[i])
 
-    # mu, tr_av_vel = sub2.k_means(tr_av['velocity']) # hold back until QW issue resolves
+    mu, tr_av_vel = sub2.k_means(tr_av['velocity'])
+    # tr_av_vel = tr_av['velocity']
 
     # %% Export tr_av and tr_av_vel to comma delimited text file
-    path_sav_vy = '/Users/hk/Desktop/LEMI/DEP-LPS/Linear EK/analysis/' + exp_date + '/vy/'
-    path_sav_tr = '/Users/hk/Desktop/LEMI/DEP-LPS/Linear EK/analysis/' + exp_date + '/tr/'
-    # tr_sav = pd.DataFrame(data = tr_av_vel, columns=['velocity']) # hold back until QW issue resolves
-    tr_sav = pd.DataFrame(data = tr_av['velocity'].values, columns=['velocity'])
-    s = path_sav_vy + '%s_R%d_Ch%02d_GFP_%02dV_20X_001.ome.csv' % (info.cond[i], info.rep[i], info.channel[i], info.voltage[i])
-    s2 = path_sav_tr + '%s_R%d_Ch%02d_GFP_%02dV_20X_001.ome.csv' % (info.cond[i], info.rep[i], info.channel[i], info.voltage[i])
+    s = path_sav_vy + '%s_R%d_Ch%02d_pH%dp%02d_%s_%02dV_10X_001.ome.txt' % (info.cond[i], info.rep[i], info.channel[i], math.floor(info.ph[i]), round(100*(info.ph[i]-math.floor(info.ph[i]))), info.light[i], info.voltage[i])
+    s2 = path_sav_tr + '%s_R%d_Ch%02d_pH%dp%02d_%s_%02dV_10X_001.ome.txt' % (info.cond[i], info.rep[i], info.channel[i], math.floor(info.ph[i]), round(100*(info.ph[i]-math.floor(info.ph[i]))), info.light[i], info.voltage[i])
+
+    tr_sav = pd.DataFrame(data = tr_av_vel, columns=['velocity'])
     tr_sav.to_csv(s, index = False)
     tr_av.to_csv(s2, index = False)
